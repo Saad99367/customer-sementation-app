1f5bd3b9a8cc36e282f1cf98da04ba4d46969717
Saad99367 <saad571369@gmail.com>
2025-07-22 18:11:43 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import joblib
+
+kmeans = joblib.load('kmeans_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+st.title('Welcome in Customer Segmentation App')
+st.write('Enter customer details to predict the segment.')
+
+age = st.number_input("Age", min_value=18, max_value=100, value=30)
+income = st.number_input("Annual Income (k$)", min_value=0, max_value=200, value=50)
+score = st.slider("Spending Score (1–100)", min_value=1, max_value=100, value=50)
+
+
+
+
+if st.button("Predict Cluster"):
+    input_data = np.array([[age, income, score]])
+    input_scaled = scaler.transform(input_data)
+    cluster = kmeans.predict(input_scaled)[0]
+    
+    st.success(f"📊 This customer belongs to Cluster #{cluster}")
